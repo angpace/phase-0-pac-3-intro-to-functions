1dc56f066df7cc54283d984ca89001070277b487
Angela Pace <angpace13@gmail.com>
2022-09-02 13:35:22 -0400

    commit tree d7308f94010d58cb32a874cabd7b29994023df3e

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Follow along with the examples here
+function doNothing () {}
+function sayHello () {
+    console.log("Hello!");
+}
+sayHello()
+function sayHelloToGuadalupe() {
+   console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe()
+function sayHelloToKevin () {
+    console.log("Hello, Kevin!");
+}
+sayHelloToKevin()
+function sayHelloToAngelaPace () {
+    console.log("Hello, Mrs.Bad Bitch");
+}
+sayHelloToAngelaPace()
+
+sayHelloToAngelaPace ()
+console.log((sayHelloToAngelaPace))
+sayHelloToGuadalupe ()
+function sayHelloTo(firstname){
+    console.log(`Hello, ${firstname}`)
+}
+
+sayHelloTo("Kevin")
+sayHelloTo("Sylvia")
+
+function say(greeting,firstname){
+    console.log (`${greeting}, ${firstname}!`)
+}
+
+say("hello", "Kevin")
+
+function add(x, y) {
+ return x + y;
+  }
+
+  console.log(add(200, 300));
+
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+
+  say("Hello, mousse")
